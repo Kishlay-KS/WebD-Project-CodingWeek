d72eec281624ef6422d0808b5a2d8ed86c8d7991
Kishlay-KS <94366817+Kishlay-KS@users.noreply.github.com>
2022-06-04 21:06:48 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,24 @@
+function search(){
+const input = document.getElementById("filter").value.toUpperCase();
+console.log(input);
+const cardContainer = document.getElementById('product-list');
+console.log(cardContainer);
+
+const cards = cardContainer.getElementsByClassName('card');
+console.log(cards);
+
+for(let i = 0;i < cards.length; i++)
+{
+    let title = cards[i].querySelector(".card-body h5.card-title");
+    console.log(title);
+
+    if(title.innerText.toUpperCase().indexOf(input) > -1)
+    {
+        cards[i].style.display = "";
+    }
+    else 
+    {
+        cards[i].style.display = "none";
+    }
+}
+}
